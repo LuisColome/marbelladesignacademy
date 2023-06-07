2524d749d10a6dd33fa8c5e3133fdb6262d5f121
Luis Colomé <hello@luiscolome.com>
2023-06-07 09:58:42 +0200

    New Kadence file to add functionalities about Kadence

---
+++ inc/kadence-changes.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Kadence changes
+ *
+ * @package     MarbellaDesignAcademy
+ * @author      Luis Colomé
+ * @since       0.8.0
+ * @license     GPL-2.0+
+**/
+
+
+// Adds post type support for exceprt. 	
+add_post_type_support( 'page', 'excerpt' );
+
+
+/* Comments
+------------------------------------------------------------------------------ */
+
+/**
+ * Remove "Says" from comments.
+ * @link https://mundogenesis.com/tutoriales-genesis/elminar-el-dicede-los-comentarios-en-genesis/
+ */
+function lcm_remove_comment_says_text() {
+    return ' ';
+}
+add_filter( 'comment_author_says_text', 'lcm_remove_comment_says_text' );
